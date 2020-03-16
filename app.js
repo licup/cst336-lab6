060070a1676a7e15433b2dc5b1e3518dc3a9e22f
EC2 Default User <ec2-user@ip-172-31-41-113.us-east-2.compute.internal>
2020-03-16 00:51:56 +0000

    lab6

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+app.engine('html', require('ejs').renderFile);
+app.use(express.static("public"));
+//routes
+app.get("/", function(req, res){
+    res.render("index.html");
+});
+app.get("/jupiter", function(req, res){
+    res.render("jupiter.html");
+});
+app.get("/mercury", function(req, res){
+    res.render("mercury.html");
+});
+app.get("/neptune", function(req, res){
+    res.render("neptune.html");
+});
+app.get("/saturn", function(req, res){
+    res.render("saturn.html");
+});
+app.get("/uranus", function(req, res){
+    res.render("uranus.html");
+});
+//server listener
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Running Express Server...");
+});
